dbc653cbb705a11a1da197f34df93e9682450db3
surajmendhe5573 <surajmendhe470@gmail.com>
2025-02-26 12:51:46 +0530

    search api added

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -13,5 +13,33 @@ const getTrendingNews= async(req, res)=>{
     }
 };
 
+const getNewsByCategory = async (req, res) => {
+    const { category } = req.params;
+    try {
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.NEWS_API_KEY}`;
+      const response = await axios.get(url);
+  
+      res.status(200).json({ message: `News for category ${category} fetched successfully`,news: response.data.articles});
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
+  };
+  
+  const searchNews = async (req, res) => {
+    const { query } = req.query;
+    try {
+      const url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${process.env.NEWS_API_KEY}`;
+      const response = await axios.get(url);
+  
+      res.status(200).json({
+        message: `Search results for ${query} fetched successfully`,
+        news: response.data.articles,
+      });
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
+  };
+  
 
-module.exports= getTrendingNews;
+
+module.exports= {getTrendingNews, getNewsByCategory, searchNews};
--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,10 @@
 const express= require('express');
-const getTrendingNews = require('../controllers/news.controller');
+const {getTrendingNews, getNewsByCategory, searchNews} = require('../controllers/news.controller');
 const router= express.Router();
 
 router.get('/trending', getTrendingNews);
+
+router.get('/category/:category', getNewsByCategory); 
+router.get('/search', searchNews); 
 
 module.exports= router;
